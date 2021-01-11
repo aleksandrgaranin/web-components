1f6b9d38a34f2b2cfc275901a7d0fda7365c6e34
aleksandGG <aleksandrgaranin89@gmail.com>
2021-01-11 11:52:49 -0800

    Using Attribute

--- tooltip.js
+++ tooltip.js
@@ -1,12 +1,16 @@
 class Tooltip extends HTMLElement {
   constructor() {
     super();
-    this._tooltipContainer
-
+    this._tooltipContainer;
+    this._tooltipText = 'some dummy text';
 
   }
 
   connectedCallback() {
+    if( this.hasAttribute('text')) {
+      this._tooltipText = this.getAttribute('text');      
+    }
+    console.log(this._tooltipText)
     const tooltipIcon = document.createElement('span');
     tooltipIcon.textContent = ' (?)';
     tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this));
@@ -17,7 +21,7 @@ class Tooltip extends HTMLElement {
 
   _showTooltip() {
     this._tooltipContainer = document.createElement('div');
-    this._tooltipContainer.textContent = 'This is the tooltip text!';
+    this._tooltipContainer.textContent = this._tooltipText;
     this.appendChild(this._tooltipContainer);
 
   }
