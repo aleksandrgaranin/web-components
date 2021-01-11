e99e9c6c0c04333febc8122c7aa66f60a640be56
aleksandGG <aleksandrgaranin89@gmail.com>
2021-01-11 12:17:49 -0800

    Styling with Shadow DOM + template + slot

--- tooltip.js
+++ tooltip.js
@@ -3,31 +3,40 @@ class Tooltip extends HTMLElement {
     super();
     this._tooltipContainer;
     this._tooltipText = 'some dummy text';
-
+    this.attachShadow({ mode: 'open' });
+    const template = document.querySelector('#tooltip-template');
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
   }
 
   connectedCallback() {
-    if( this.hasAttribute('text')) {
-      this._tooltipText = this.getAttribute('text');      
+    if (this.hasAttribute('text')) {
+      this._tooltipText = this.getAttribute('text');
     }
     console.log(this._tooltipText)
-    const tooltipIcon = document.createElement('span');
-    tooltipIcon.textContent = ' (?)';
+    // const tooltipIcon = document.createElement('span');
+    // tooltipIcon.textContent = ' (?)';
+    const tooltipIcon = this.shadowRoot.querySelector('span');
     tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this));
     tooltipIcon.addEventListener('mouseleave', this._hloseTooltip.bind(this));
-    this.appendChild(tooltipIcon);
+    this.shadowRoot.appendChild(tooltipIcon);
+    this.style.position = 'relative'
     // console.log('It is working');
   }
 
   _showTooltip() {
     this._tooltipContainer = document.createElement('div');
     this._tooltipContainer.textContent = this._tooltipText;
-    this.appendChild(this._tooltipContainer);
+    this._tooltipContainer.style.backgroundColor = 'black';
+    this._tooltipContainer.style.color = 'white';
+    this._tooltipContainer.style.position = 'absolute';
+    this._tooltipContainer.style.zIndex = '10';
+
+    this.shadowRoot.appendChild(this._tooltipContainer);
 
   }
 
   _hloseTooltip() {
-    this.removeChild(this._tooltipContainer);
+    this.shadowRoot.removeChild(this._tooltipContainer);
   }
 
   disconnectedCallback() {
