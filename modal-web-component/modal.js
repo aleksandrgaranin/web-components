847d7724bb4443057c49562d324824d634bf87bf
aleksandGG <aleksandrgaranin89@gmail.com>
2021-01-12 14:48:13 -0800

    finished component

--- modal-web-component/modal.js
+++ modal-web-component/modal.js
@@ -23,9 +23,13 @@ class Modal extends HTMLElement {
           pointer-events: all;
         }
 
+        :host([opened]) #modal {
+          top: 15vh;
+        }
+
         #modal {
           position: fixed;
-          top: 15vh;
+          top: 10vh;
           left: 25%;
           width: 50%;
           z-index: 100;
@@ -37,6 +41,7 @@ class Modal extends HTMLElement {
           justify-content: space-between;
           opacity: 0;
           pointer-events: none;
+          transition: all 0.3s ease-out;
         }
 
         #main {
@@ -52,10 +57,12 @@ class Modal extends HTMLElement {
 
         header {
           padding: 1rem;
+          border-bottom: 1px solid #ccc;
         }
 
         ::slotted(h1) {
           font-size: 1.50rem;
+          margin: 0;
         }
 
         #actions button {
@@ -83,9 +90,11 @@ class Modal extends HTMLElement {
     });
     const cancelButton = this.shadowRoot.querySelector('#cancel-btn');
     const confirmButton = this.shadowRoot.querySelector('#confirm-btn');
+    const backdrop = this.shadowRoot.querySelector('#backdrop');
 
-    cancelButton.addEventListener('click', this._cancel.bind(this))
-    confirmButton.addEventListener('click', this._confirm.bind(this))
+    cancelButton.addEventListener('click', this._cancel.bind(this));
+    confirmButton.addEventListener('click', this._confirm.bind(this));
+    backdrop.addEventListener('click', this._cancel.bind(this));
 
   }
 
